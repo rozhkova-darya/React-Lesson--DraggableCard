3fbb6538fcc606ff8872cd5ed707e51260a5b7c9
Darya Rozhkova <drakona-97@mail.ru>
2022-06-29 11:16:05 +0300

    finish the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import randomColor from 'randomcolor';
+import Draggable from 'react-draggable';
+
 import './App.css';
 
 function App() {
+
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  );
+
+  useEffect(() => {
+     localStorage.setItem('items', JSON.stringify(items))
+  }, [items]);
+
+  const newItem = () => {
+    if(item.trim() !== '') {
+      const nItem = {
+        id: uuidv4(),
+        item,
+        color: randomColor({
+          luminosity: 'light',
+        }),
+        defaultPos: {
+          x: 0, y: 0,
+        }
+      }
+      setItems([...items, nItem]); 
+    }
+    
+    setItem('');
+  };
+
+  const deliteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+  const updatePos = (data, index) => {
+    let newArrayItems = [...items];
+    newArrayItems[index].defaultPos = {x: data.x, y: data.y}
+    setItems(newArrayItems)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <input className='add_input' type="text" placeholder='Создай карточку' 
+          onChange = {(e) => setItem(e.target.value)} value={item}
+          onKeyPress={(e) => e.code === 'Enter' && newItem()}
+        />
+        <button className='add_btn' onClick={newItem}>Добавить</button>
+      </div>
+
+      {
+        items.map((item, index) => {
+          return <Draggable
+              key={index}
+              defaultPosition={item.defaultPos}
+              onStop={(e, data) => updatePos(data, index)}
+            >
+
+            <div className='todo__item' style={{backgroundColor: item.color}}>
+              {`${item.item}`}
+              <button className='del_btn' onClick={() => deliteItem(item.id)}>X</button>
+            </div>
+
+            </Draggable>
+        })
+      }
+    </>
   );
 }
 
